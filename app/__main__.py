f57c385e35d903980808e97ad471910d964c5afa
KisSCoOl <kisscool@kisscool.net>
2026-06-15 17:52:37 +0200

    fix(app): fix app icon

--- app/__main__.py
+++ app/__main__.py
@@ -2,15 +2,20 @@ import sys
 
 from PyQt6.QtWidgets import QApplication
 
+from app.gui.app_icon import apply_app_icon
+from app.gui.app_icon import prepare_platform
 from app.gui.main_window import MainWindow
 from app.gui.ui import styles
 
 
 def main():
+    prepare_platform()
     app = QApplication(sys.argv)
     styles.apply_app_theme(app)
+    apply_app_icon(app)
 
     window = MainWindow()
+    apply_app_icon(app, window)
     window.show()
 
     app.exec()
--- app/constants.py
+++ app/constants.py
@@ -7,6 +7,7 @@ APP_VERSION = version("pysteg")
 SHARED_SECRET_MIN_LENGTH = 8
 
 ASSETS_DIRECTORY: Path = Path(__file__).parent.parent / "assets"
+APP_ICON: Path = ASSETS_DIRECTORY / "icons" / "icon.png"
 
 ICON_LOCK = "🔐"
 ICON_UNLOCK = "🔓"
