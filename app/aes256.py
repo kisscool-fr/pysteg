fbca47b9a9f43e169835452518b3e032c725dee4
KisSCoOl <kisscool@kisscool.net>
2026-06-17 15:35:16 +0200

    chore(nit): nitpick rename

--- app/aes256.py
+++ app/aes256.py
@@ -5,10 +5,11 @@ from cryptography.exceptions import InvalidTag
 from cryptography.hazmat.primitives.ciphers.aead import AESGCM
 from cryptography.hazmat.primitives.kdf.argon2 import Argon2id
 
-SALT_SIZE = 16
-NONCE_SIZE = 12
+# Constants for AES-256 encryption
+_SALT_SIZE = 16
+_NONCE_SIZE = 12
 
-# memory_cost: 64 MiB, time_cost: 3 passes, lanes: 1 thread.
+# Argon2 parameters: memory_cost: 64 MiB, time_cost: 3 passes, lanes: 1 thread.
 _ARGON2_MEMORY_COST = 65536
 _ARGON2_TIME_COST = 3
 _ARGON2_LANES = 1
@@ -21,7 +22,7 @@ class Crypto:
         self._salt: bytes = self.get_random_salt()
 
     def get_random_salt(self) -> bytes:
-        return urandom(SALT_SIZE)
+        return urandom(_SALT_SIZE)
 
     def derive_key(self, secret: bytes, *, salt: bytes | None = None) -> bytes:
         kdf = Argon2id(
@@ -34,7 +35,7 @@ class Crypto:
         return kdf.derive(secret)
 
     def encrypt(self, value: str) -> str:
-        nonce = urandom(NONCE_SIZE)
+        nonce = urandom(_NONCE_SIZE)
 
         derived_key = self.derive_key(self._secret)
         aesgcm = AESGCM(derived_key)
@@ -47,9 +48,9 @@ class Crypto:
         try:
             encrypted_data = base64.b64decode(value.encode("utf-8"))
 
-            salt = encrypted_data[:SALT_SIZE]
-            nonce = encrypted_data[SALT_SIZE : SALT_SIZE + NONCE_SIZE]
-            ciphertext = encrypted_data[SALT_SIZE + NONCE_SIZE :]
+            salt = encrypted_data[:_SALT_SIZE]
+            nonce = encrypted_data[_SALT_SIZE : _SALT_SIZE + _NONCE_SIZE]
+            ciphertext = encrypted_data[_SALT_SIZE + _NONCE_SIZE :]
 
             derived_key = self.derive_key(self._secret, salt=salt)
             aesgcm = AESGCM(derived_key)
